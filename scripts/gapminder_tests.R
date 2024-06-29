0abb25ad6140a30ddf1fa4bd487f3881e8c8461b
MarvinKweyu <mkweyu1@gmail.com>
2024-06-29 15:27:49 +0300

    update test for two continents

--- scripts/gapminder_tests.R
+++ scripts/gapminder_tests.R
@@ -8,3 +8,9 @@ one_way <- gapminder |>
   select(lifeExp) |> 
   t.test(mu=50)
 one_way
+
+# two continents t-test
+gapminder |> 
+  filter(continent %in% c("Africa", "Europe")) %>%
+  {t.test(lifeExp ~ continent, data = ., alternative='two.sided')}
+  
